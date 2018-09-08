9645baa431d77f0ba8d8b90daf37823e96964b2e
DillonPu <dillonpu@foxmail.com>
2018-09-08 10:09:57 +0800

    Delete itcaht.py

--- itchat/itcaht.py
+++
@@ -1,7 +0,0 @@
-import itchat
-@itchat.msg_register(itchat.content.TEXT)
-def print_content(msg):
-    print(msg['Text'])
-    itchat.send(msg['Text'],"filehelper")
-itchat.auto_login()
-itchat.run()
